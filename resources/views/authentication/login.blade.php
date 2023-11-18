6d67f21c466f9008f50c1bbf3f2e58280db773e1
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2023-11-18 23:09:38 +0900

    Progress: Finish Statis Login Page

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -2,34 +2,48 @@
 
 @section('main.authentication')
     <main class="authentication">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-6">
+        <div class="container-fluid position-relative" style="height: 100%;">
+            <div class="row" style="height: 100%;">
+                <div class="col-lg-6 p-0 d-flex align-items-center position-relative wrapper-style">
+                    <a class="logo-brand">
+                        <img src="{{ asset('assets/images/brand/logo-brand.svg') }}" alt="Logo Brand" class="img-fluid">
+                    </a>
                     <div class="content-authentication">
                         <h1 class="title">Login</h1>
                         <form class="form">
-                            <div class="input-group">
-                                <input type="email" id="email">
-                                <label for="email d-flex align-items-center gap-2">
-                                    <img src="{{ asset('assets/images/icons/email.svg') }}" alt="Email Icon">
-                                    <span>Enter your email</span>
-                                </label>
+                            <div class="row flex-column">
+                                <div class="col-lg-9 col-xl-8 col-xxl-7 mb-2">
+                                    <div class="input-group">
+                                        <input type="email" id="email" placeholder=" ">
+                                        <label for="email" class="d-flex align-items-center">
+                                            <img src="{{ asset('assets/images/icons/email.svg') }}" alt="Email Icon">
+                                            <div class="line"></div>
+                                            <span>Enter your email</span>
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-lg-9 col-xl-8 col-xxl-7 mb-4">
+                                    <div class="input-group">
+                                        <input type="password" id="password" placeholder=" ">
+                                        <label for="password" class="d-flex align-items-center">
+                                            <img src="{{ asset('assets/images/icons/password.svg') }}" alt="Password Icon">
+                                            <div class="line"></div>
+                                            <span>Enter your password</span>
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <button type="submit" class="button-primary d-flex align-items-center">
+                                        <span>Login account</span>
+                                        <img src="{{ asset('assets/images/icons/arrow-light.svg') }}" alt="Arrow Button Light">
+                                    </button>
+                                </div>
                             </div>
-                            <div class="input-group">
-                                <input type="email" id="email">
-                                <label for="email d-flex align-items-center gap-2">
-                                    <img src="{{ asset('assets/images/icons/email.svg') }}" alt="Email Icon">
-                                    <span>Enter your email</span>
-                                </label>
-                            </div>
-                            <button type="submit">
-                                <span>Login account</span>
-                                <img src="{{ asset('assets/images/icons/arrow-light.svg') }}" alt="Arrow Button Light">
-                            </button>
                         </form>
+                        <p class="link-redirect">Don't have an account? <a href="#">Register for Free</a></p>
                     </div>
                 </div>
-                <div class="col-6 p-0 d-flex align-items-center">
+                <div class="col-6 p-0 d-flex align-items-center d-none d-lg-inline-block">
                     <img src="{{ asset('assets/images/banners/banner-login.jpg') }}" alt="Login Banner" class="img-fluid banner-login">
                 </div>
             </div>
